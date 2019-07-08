fcb2f12c2335c46ff157bffd3a2a5d8986b0801a
turnerm <mturner@lco.global>
2019-07-08 22:47:50 +0200

    save as you go

--- scrape_newspapers/inspect_articles_tag_topical.py
+++ scrape_newspapers/inspect_articles_tag_topical.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python
-# coding: utf8
-from __future__ import unicode_literals, print_function
-
-import plac
 import os
 import importlib
 
+import plac
 import pandas as pd
 
 utils = importlib.import_module('utils')
@@ -14,62 +10,78 @@ pd.set_option('display.max_columns', 5)
 pd.set_option('max_colwidth', 18)
 pd.set_option('expand_frame_repr', True)
 
-MAX_ARTICLES_PER_NEWSPAPER = 5
-
 
 @plac.annotations(
     config_file="Configuration file",
+    recreate_summary_file=("Recreate the summary file", "flag", "r")
 )
-def main(config_file):
+def main(config_file,
+         recreate_summary_file=False):
     """
     Inspect articles and decide if relevant
     add corresponding boolean (topical) to dataframe
     """
-
     config = utils.get_config(config_file)
-    articles_folder = utils.get_scraped_article_output_dir(config)
-
-    files_in_articles_folder = os.listdir(articles_folder)
-    files_in_articles_folder = [f for f in files_in_articles_folder if '.csv' in f]
-    df_articles_topical_list = []
-
-    # Get number of articles so that you know what you're in for
-    num_articles = 0
-    for file in files_in_articles_folder:
-        num_articles += len(pd.read_csv(os.path.join(articles_folder, file),
-                                        sep='|')[:MAX_ARTICLES_PER_NEWSPAPER])
-    print('Analyzing {} articles'.format(num_articles))
-
-    cnt_article = 0
-    for file in files_in_articles_folder:
-        df_articles = pd.read_csv(articles_folder+'/'+file,
-                                  sep='|')[:MAX_ARTICLES_PER_NEWSPAPER]
-        print("Analysing newspaper: {0}".format(file))
-        var_topical_bool = []
-
-        for index, article in df_articles.iterrows():
-            print("\nArticle #{number}: {title}".format(
-                number=cnt_article+1, title=article['title']))
-            var_topical = input("Is it topical? t (True), f (False), i (Inspect text)  ")
-            if var_topical == 'i':
-                print(article['text'])
-                var_topical = input("Is it topical? t (True), f (False)  ")
-            var_topical_bool.append(var_topical=='t')
-            cnt_article += 1
-
-        df_articles = df_articles.assign(topical = var_topical_bool)
-        print(df_articles.head())
-        df_articles_topical_list.append(df_articles)
 
     output_directory = utils.INPSECTED_ARTICLES_OUTPUT_DIR
-    print("Saving all articles to {0}".format(output_directory))
-    # create output dir if it doesn't exist
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
+    articles_folder = utils.get_scraped_article_output_dir(config)
+    files_in_articles_folder = os.listdir(articles_folder)
+    files_in_articles_folder = [f for f in files_in_articles_folder if '.csv' in f]
+
+    # If doesn't exist yet, make a summary of articles to keep track of those
+    # which have been tagged
+    articles_summary_filename = os.path.join(output_directory,
+                                             utils.get_articles_summary_output_filename(config))
+    if os.path.isfile(articles_summary_filename) and not recreate_summary_file:
+        print('Reading in summary file {}'.format(articles_summary_filename))
+        df_articles_summary = pd.read_csv(articles_summary_filename)
+    else:
+        print('Creating summary file {}'.format(articles_summary_filename))
+        columns = ['newspaper', 'article_number', 'title', 'topical']
+        df_articles_summary = pd.DataFrame(columns=columns)
+        index_article = 0
+        for file in files_in_articles_folder:
+            df_articles = pd.read_csv(articles_folder+'/'+file, sep='|')
+            for index, article in df_articles.iterrows():
+                row_to_add = [file, index, article['title'], None]
+                df_articles_summary.loc[index_article] = row_to_add
+                index_article += 1
+        df_articles_summary.to_csv(articles_summary_filename)
+
+    articles_to_analyze = df_articles_summary.loc[pd.isna(df_articles_summary['topical'])]
+    number_to_analyze = len(articles_to_analyze)
+    print('Analyzing {} articles'.format(number_to_analyze))
+
+    # Tag articles and save it in the summary
+    cnt_article = 0
+    for _, row in articles_to_analyze.iterrows():
+        article = pd.read_csv(articles_folder+'/'+row['newspaper'], sep='|').iloc[row['article_number']]
+        print("\nArticle #{number}/{total}: {title}".format(
+            number=cnt_article+1, total=number_to_analyze, title=article['title']))
+        var_topical = input("Is it topical? t (True), f (False), i (Inspect text), q (Quit)  ")
+        if var_topical == 'q':
+            break
+        elif var_topical == 'i':
+            print(article['text'])
+            var_topical = input("Is it topical? t (True), f (False)  ")
+        df_articles_summary.loc[row[0], 'topical'] = var_topical == 't'
+        cnt_article += 1
+        df_articles_summary.to_csv(articles_summary_filename)
+
+    # Use the summary to create a csv with only relevant articles
+    df_articles_topical_list = []
+    relevant_articles = df_articles_summary[df_articles_summary['topical'] == True]
+    print('Saving {} relevent articles'.format(len(relevant_articles)))
+    for _, row in articles_to_analyze.iterrows():
+        article = pd.read_csv(articles_folder+'/'+row['newspaper'], sep='|').iloc[row['article_number']]
+        df_articles_topical_list.append(article)
+    print("Saving all articles to {0}".format(output_directory))
     output_filename = utils.get_inspected_articles_output_filename(config)
     df_articles_topical = pd.concat(df_articles_topical_list, axis=0)
-    df_articles_topical.to_csv(os.path.join(output_directory, output_filename), sep='|')
+    df_articles_topical.to_csv(os.path.join(output_directory, output_filename), sep='|', header=True)
 
 
 if __name__ == '__main__':
