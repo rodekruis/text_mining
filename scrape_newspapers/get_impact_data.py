7010c8e1e13af365ecf3d51f02001469a0c56f1d
turnerm <mturner@lco.global>
2019-08-06 10:50:05 +0200

    Added output directory arg

--- scrape_newspapers/get_impact_data.py
+++ scrape_newspapers/get_impact_data.py
@@ -9,14 +9,16 @@ ImpactTableGenerator = importlib.import_module('ImpactTableGenerator')
 @plac.annotations(
     config_file="Configuration file",
     input_filename=("Optional input filename", "option", "i", str),
-    output_filename_base=("Optional output filename base", "option", "o", str)
+    output_filename_base=("Optional output filename base", "option", "o", str),
+    output_directory=("Optional output directory", "option", "d", str)
 )
-def main(config_file, input_filename=None, output_filename_base=None):
+def main(config_file, input_filename=None, output_filename_base=None, output_directory=None):
 
     impact_table_generator = ImpactTableGenerator.ImpactTableGenerator(
         config_file,
         input_filename=input_filename,
-        output_filename_base=output_filename_base)
+        output_filename_base=output_filename_base,
+        output_directory=output_directory)
 
     impact_table_generator.loop_over_articles()
 
