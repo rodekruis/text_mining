75de52e6b0830aba7ace71e37fe9acfe882a1376
Bonnie <bvanvuure@rodekruis.nl>
2019-08-14 11:13:59 +0200

    make ready for PR

--- scrape_newspapers/inspect_articles_tag_topical.py
+++ scrape_newspapers/inspect_articles_tag_topical.py
@@ -23,21 +23,18 @@ def _load_locations(country, country_short):
     locations_df = pd.read_csv(input_file, sep='\t', encoding='utf-8', usecols=columns)
     return locations_df
 
-@plac.annotations(
-    #config_file="Configuration file",
+@plac.annotations(config_file="Configuration file",
     recreate_summary_file=("Recreate the summary file", "flag", "r")
 )
-def main(
-        #config_file,
+def main(config_file,
          recreate_summary_file=False):
     """
     Inspect articles and decide if relevant
     add corresponding boolean (topical) to dataframe
     """
-    config_file = 'config_files/mali.cfg'
+    #config_file = 'config_files/mali.cfg'
     config = utils.get_config(config_file)
     keywords = utils.get_keywords(config_file)
-    recreate_summary_file = True
 
     output_directory = utils.INPSECTED_ARTICLES_OUTPUT_DIR
     if not os.path.exists(output_directory):
@@ -113,43 +110,43 @@ def main(
         df_articles_summary.to_csv(articles_summary_filename, index=False)
         
         #debugging, find word that triggers annotation choice:
-#        test_title = 'Inondation meurtrière à Bamako: Les maires à l’indexe – MALI 24 INFO'
-#        print([word for word in keys_not_topical if word.lower() in test_title.lower()])
+        #test_title = 'Inondation meurtrière à Bamako: Les maires à l’indexe – MALI 24 INFO'
+        #print([word for word in keys_not_topical if word.lower() in test_title.lower()])
 
 
-#    articles_to_analyze = df_articles_summary.loc[pd.isna(df_articles_summary['topical'])]
-#    number_to_analyze = len(articles_to_analyze)
-#    print('/n Analyzing {} articles'.format(number_to_analyze))
-#
-#    # Tag articles and save it in the summary
-#    cnt_article = 0
-#    for row in articles_to_analyze.itertuples():
-#        newspaper = articles_to_analyze.at[row.Index, 'newspaper']
-#        article_number = articles_to_analyze.at[row.Index, 'article_number']
-#        article = pd.read_csv(articles_folder+'/'+newspaper, sep='|').iloc[article_number]
-#        print("\nArticle #{number}/{total}: {title}".format(
-#            number=cnt_article+1, total=number_to_analyze, title=article['title']))
-#        var_topical = input("Is it topical? t (True), f (False), i (Inspect text), q (Quit)  ")
-#        if var_topical == 'q':
-#            break
-#        elif var_topical == 'i':
-#            print(article['text'])
-#            var_topical = input("Is it topical? t (True), f (False)  ")
-#        df_articles_summary.loc[row.Index, 'topical'] = var_topical == 't'
-#        cnt_article += 1
-#        df_articles_summary.to_csv(articles_summary_filename, index=False)
-#
-#    # Use the summary to create a csv with only relevant articles
-#    columns = ['Unnamed: 0', 'title', 'publish_date', 'text', 'url']
-#    df_articles_topical = pd.DataFrame(columns=columns)
-#    relevant_articles = df_articles_summary[df_articles_summary['topical'] == True]
-#    print('Saving {} topical articles'.format(len(relevant_articles)))
-#    for index, row in relevant_articles.iterrows():
-#        article = pd.read_csv(articles_folder+'/'+row['newspaper'], sep='|').iloc[row['article_number']]
-#        df_articles_topical.loc[index] = article
-#    print("Saving all articles to {0}".format(output_directory))
-#    output_filename = utils.get_inspected_articles_output_filename(config)
-#    df_articles_topical.to_csv(os.path.join(output_directory, output_filename), sep='|', header=True)
+    articles_to_analyze = df_articles_summary.loc[pd.isna(df_articles_summary['topical'])]
+    number_to_analyze = len(articles_to_analyze)
+    print('/n Analyzing {} articles'.format(number_to_analyze))
+
+    # Tag articles and save it in the summary
+    cnt_article = 0
+    for row in articles_to_analyze.itertuples():
+        newspaper = articles_to_analyze.at[row.Index, 'newspaper']
+        article_number = articles_to_analyze.at[row.Index, 'article_number']
+        article = pd.read_csv(articles_folder+'/'+newspaper, sep='|').iloc[article_number]
+        print("\nArticle #{number}/{total}: {title}".format(
+            number=cnt_article+1, total=number_to_analyze, title=article['title']))
+        var_topical = input("Is it topical? t (True), f (False), i (Inspect text), q (Quit)  ")
+        if var_topical == 'q':
+            break
+        elif var_topical == 'i':
+            print(article['text'])
+            var_topical = input("Is it topical? t (True), f (False)  ")
+        df_articles_summary.loc[row.Index, 'topical'] = var_topical == 't'
+        cnt_article += 1
+        df_articles_summary.to_csv(articles_summary_filename, index=False)
+
+    # Use the summary to create a csv with only relevant articles
+    columns = ['Unnamed: 0', 'title', 'publish_date', 'text', 'url']
+    df_articles_topical = pd.DataFrame(columns=columns)
+    relevant_articles = df_articles_summary[df_articles_summary['topical'] == True]
+    print('Saving {} topical articles'.format(len(relevant_articles)))
+    for index, row in relevant_articles.iterrows():
+        article = pd.read_csv(articles_folder+'/'+row['newspaper'], sep='|').iloc[row['article_number']]
+        df_articles_topical.loc[index] = article
+    print("Saving all articles to {0}".format(output_directory))
+    output_filename = utils.get_inspected_articles_output_filename(config)
+    df_articles_topical.to_csv(os.path.join(output_directory, output_filename), sep='|', header=True)
 
 
 if __name__ == '__main__':
