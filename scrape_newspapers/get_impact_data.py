2c6b2a2805dc38068ecd90e8cbdad310af2c9d40
turnerm <mturner@lco.global>
2019-08-14 13:59:50 +0200

    Added debug command line arg

--- scrape_newspapers/get_impact_data.py
+++ scrape_newspapers/get_impact_data.py
@@ -1,15 +1,20 @@
 import plac
 
+
 from impact_table_generator import ImpactTableGenerator
+from utils import utils
 
 
 @plac.annotations(
     config_file="Configuration file",
     input_filename=("Optional input filename", "option", "i", str),
     output_filename_base=("Optional output filename base", "option", "o", str),
-    output_directory=("Optional output directory", "option", "d", str)
+    output_directory=("Optional output directory", "option", "d", str),
+    debug=("Debug", "flag", "debug")
 )
-def main(config_file, input_filename=None, output_filename_base=None, output_directory=None):
+def main(config_file, input_filename=None, output_filename_base=None, output_directory=None, debug=False):
+    log_level = 'DEBUG' if debug else 'INFO'
+    utils.set_log_level(log_level)
 
     impact_table_generator = ImpactTableGenerator.ImpactTableGenerator(
         config_file,
